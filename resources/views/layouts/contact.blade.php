8b5e4c4d96b95a6dd870490fc48b3f18ee54173e
krizedward <edward.mangare@binus.ac.id>
2020-08-19 23:28:02 +0800

    (v.3.4.3.109)_update_contact

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -3,3 +3,17 @@
 @section('title','Contact Us')
 
 @include('layouts.css_and_js.dashboard')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="alert alert-success alert-dismissable">
+            <h4>
+                <i class="icon fa fa-comments"></i>
+                Email : Nusia.id@gmail.com
+            </h4>
+            Jika mengalami kendala atau ada pertanyaan silahkan hubungi ke email kami
+        </div>
+    </div>
+</div>
+@stop
