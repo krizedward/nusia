7aee1fe5bd9b84a76c756b5783206d00f8cbead7
imanueltantodaud <imanuel.tanto.daud@gmail.com>
2020-08-25 01:55:06 +0700

    (v.3.4.3.189)_menghapus_file_lama

--- resources/views/(v.3.4.2.17)_file/layouts/admin/_head.blade.php
+++
@@ -1,7 +0,0 @@
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Nusia | @yield('title')</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
-  @stack('style')
