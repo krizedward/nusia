f0d241c251a1083c06e695057ba29a4e216d4747
imanueltantodaud <imanuel.tanto.daud@gmail.com>
2020-08-25 16:05:15 +0700

    (v.3.4.3.199)_update_material_publics_database_seeder_dan_konten_materi

--- database/seeds/MaterialPublicsTableSeeder.php
+++ database/seeds/MaterialPublicsTableSeeder.php
@@ -22,52 +22,67 @@ class MaterialPublicsTableSeeder extends Seeder
         MaterialPublic::create([
             'code' => 'MP-0000002',
             'course_package_id' => 1,
-            'name' => 'PRONUNCIATION',
+            'name' => 'ALPHABET',
             'description' => null,
-            'path' => '2_PRONUNCIATION.pdf',
+            'path' => '2_ALPHABET.pdf',
         ]);
-        /*MaterialPublic::create([
-            'code' => 'MP-001',
+        MaterialPublic::create([
+            'code' => 'MP-0000003',
             'course_package_id' => 1,
-            'name' => 'Material Public 001',
+            'name' => 'VOCAB (2nd MEETING)',
             'description' => null,
-            'path' => null,
+            'path' => '3_VOCAB (2nd MEETING).pdf',
         ]);
         MaterialPublic::create([
-            'code' => 'MP-002',
-            'course_package_id' => 14,
-            'name' => 'Material Public 002',
-            'description' => null,
-            'path' => null,
-        ]);
-        MaterialPublic::create([
-            'code' => 'MP-003',
-            'course_package_id' => 4,
-            'name' => 'Material Public 003',
-            'description' => null,
-            'path' => null,
-        ]);
-        MaterialPublic::create([
-            'code' => 'MP-004',
-            'course_package_id' => 77,
-            'name' => 'Material Public 004',
-            'description' => null,
-            'path' => null,
-        ]);
-        MaterialPublic::create([
-            'code' => 'MP-005',
-            'course_package_id' => 10,
-            'name' => 'Material Public 005',
-            'description' => null,
-            'path' => null,
-        ]);*/
-        /*MaterialPublic::create([
-            'code' => 'MP-001',
+            'code' => 'MP-0000004',
             'course_package_id' => 1,
-            'name' => 'test',
-            'description' => 'test',
-            'path' => 'test',
-        ]);*/
+            'name' => 'VOCAB (3rd MEETING)',
+            'description' => null,
+            'path' => '4_VOCAB (3rd MEETING).pdf',
+        ]);
+        MaterialPublic::create([
+            'code' => 'MP-0000005',
+            'course_package_id' => 2,
+            'name' => 'MiraLesmana',
+            'description' => null,
+            'path' => '5_MiraLesmana.pdf',
+        ]);
+        MaterialPublic::create([
+            'code' => 'MP-0000006',
+            'course_package_id' => 2,
+            'name' => 'WORKSHEET (1st meeting)',
+            'description' => null,
+            'path' => '6_WORKSHEET (1st meeting).pdf',
+        ]);
+        MaterialPublic::create([
+            'code' => 'MP-0000007',
+            'course_package_id' => 2,
+            'name' => 'WORKSHEET (2nd meeting)',
+            'description' => null,
+            'path' => '7_WORKSHEET (2nd meeting).pdf',
+        ]);
+        MaterialPublic::create([
+            'code' => 'MP-0000008',
+            'course_package_id' => 2,
+            'name' => 'Teks minggu 3',
+            'description' => null,
+            'path' => '8_Teks minggu 3.pdf',
+        ]);
+        MaterialPublic::create([
+            'code' => 'MP-0000009',
+            'course_package_id' => 2,
+            'name' => 'WORKSHEET (3rd meeting)',
+            'description' => null,
+            'path' => '9_WORKSHEET (3rd meeting).pdf',
+        ]);
+        MaterialPublic::create([
+            'code' => 'MP-0000010',
+            'course_package_id' => 3,
+            'name' => 'WORKSHEET (2nd meeting)',
+            'description' => null,
+            'path' => '10_WORKSHEET (2nd meeting).pdf',
+        ]);
+
         /*
         factory(App\Models\MaterialPublic::class, 243)
             ->states('Randomized')
