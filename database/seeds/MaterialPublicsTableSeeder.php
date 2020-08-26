fb4d4446280aaea1e0460b1503639e9a8942288a
imanueltantodaud <imanuel.tanto.daud@gmail.com>
2020-08-26 08:23:44 +0700

    (v.3.4.3.211)_update_database_seeder

--- database/seeds/MaterialPublicsTableSeeder.php
+++ database/seeds/MaterialPublicsTableSeeder.php
@@ -15,79 +15,79 @@ class MaterialPublicsTableSeeder extends Seeder
         MaterialPublic::create([
             'code' => 'MP-0000001',
             'course_package_id' => 1,
-            'name' => 'CLASSROOM INSTRUCTIONS',
+            'name' => 'Session 1 - CLASSROOM INSTRUCTIONS',
             'description' => null,
-            'path' => '1_CLASSROOM INSTRUCTIONS.pdf',
+            'path' => '1_Session 1 - CLASSROOM INSTRUCTIONS.pdf',
         ]);
         MaterialPublic::create([
             'code' => 'MP-0000002',
             'course_package_id' => 1,
-            'name' => 'ALPHABET',
+            'name' => 'Session 1 - ALPHABET',
             'description' => null,
-            'path' => '2_ALPHABET.pdf',
+            'path' => '2_Session 1 - ALPHABET.pdf',
         ]);
         MaterialPublic::create([
             'code' => 'MP-0000003',
             'course_package_id' => 1,
-            'name' => 'VOCAB (2nd MEETING)',
+            'name' => 'Session 2 - VOCAB (2nd MEETING)',
             'description' => null,
-            'path' => '3_VOCAB (2nd MEETING).pdf',
+            'path' => '3_Session 2 - VOCAB (2nd MEETING).pdf',
         ]);
         MaterialPublic::create([
             'code' => 'MP-0000004',
             'course_package_id' => 1,
-            'name' => 'VOCAB (3rd MEETING)',
+            'name' => 'Session 3 - VOCAB (3rd MEETING)',
             'description' => null,
-            'path' => '4_VOCAB (3rd MEETING).pdf',
+            'path' => '4_Session 3 - VOCAB (3rd MEETING).pdf',
         ]);
         MaterialPublic::create([
             'code' => 'MP-0000005',
             'course_package_id' => 2,
-            'name' => 'MiraLesmana',
+            'name' => 'Session 1 - MiraLesmana',
             'description' => null,
-            'path' => '5_MiraLesmana.pdf',
+            'path' => '5_Session 1 - MiraLesmana.pdf',
         ]);
         MaterialPublic::create([
             'code' => 'MP-0000006',
             'course_package_id' => 2,
-            'name' => 'WORKSHEET (1st meeting)',
+            'name' => 'Session 1 - WORKSHEET (1st meeting)',
             'description' => null,
-            'path' => '6_WORKSHEET (1st meeting).pdf',
+            'path' => '6_Session 1 - WORKSHEET (1st meeting).pdf',
         ]);
         MaterialPublic::create([
             'code' => 'MP-0000007',
             'course_package_id' => 2,
-            'name' => 'WORKSHEET (2nd meeting)',
+            'name' => 'Session 2 - WORKSHEET (2nd meeting)',
             'description' => null,
-            'path' => '7_WORKSHEET (2nd meeting).pdf',
+            'path' => '7_Session 2 - WORKSHEET (2nd meeting).pdf',
         ]);
         MaterialPublic::create([
             'code' => 'MP-0000008',
             'course_package_id' => 2,
-            'name' => 'Teks minggu 3',
+            'name' => 'Session 3 - Teks minggu 3',
             'description' => null,
-            'path' => '8_Teks minggu 3.pdf',
+            'path' => '8_Session 3 - Teks minggu 3.pdf',
         ]);
         MaterialPublic::create([
             'code' => 'MP-0000009',
             'course_package_id' => 2,
-            'name' => 'WORKSHEET (3rd meeting)',
+            'name' => 'Session 3 - WORKSHEET (3rd meeting)',
             'description' => null,
-            'path' => '9_WORKSHEET (3rd meeting).pdf',
+            'path' => '9_Session 3 - WORKSHEET (3rd meeting).pdf',
         ]);
         MaterialPublic::create([
             'code' => 'MP-0000010',
             'course_package_id' => 3,
-            'name' => 'WORKSHEET (2nd meeting)',
+            'name' => 'Session 2 - WORKSHEET (2nd meeting)',
             'description' => null,
-            'path' => '10_WORKSHEET (2nd meeting).pdf',
+            'path' => '10_Session 2 - WORKSHEET (2nd meeting).pdf',
         ]);
         MaterialPublic::create([
             'code' => 'MP-0000011',
             'course_package_id' => 3,
-            'name' => 'WORKSHEET (3rd meeting)',
+            'name' => 'Session 3 - WORKSHEET (3rd meeting)',
             'description' => null,
-            'path' => '11_WORKSHEET (3rd meeting).pdf',
+            'path' => '11_Session 3 - WORKSHEET (3rd meeting).pdf',
         ]);
 
         /*
