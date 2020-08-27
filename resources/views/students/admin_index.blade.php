859d47614d0aa42b2c6f7f53683535e45cd694a8
imanueltantodaud <imanuel.tanto.daud@gmail.com>
2020-08-27 08:47:44 +0700

    (v.3.4.3.226)_update_tampilan_admin_untuk_view_student_index

--- resources/views/students/admin_index.blade.php
+++ resources/views/students/admin_index.blade.php
@@ -2,7 +2,9 @@
 
 @section('title','Admin | Student')
 
-@include('layouts.css_and_js.form_general')
+{{-- @include('layouts.css_and_js.form_general') --}}
+
+@include('layouts.css_and_js.table')
 
 @section('content-header')
     <h1><b>Student</b></h1>
