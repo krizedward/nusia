30584890d05e3166c96165fa431f719429a3426c
imanueltantodaud <imanuel.tanto.daud@gmail.com>
2021-01-12 23:19:47 +0700

    (v.3.4.4.107)_update_language_translation_preparation

---
+++ resources/lang/en/custom_dashboard.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'panel_time' => [
+        'wib_text' => 'Western Indonesian time:',
+        'other_tz_text' => 'Your time:',
+    ],
+    'free_classes' => [
+        'sentence_1' => 'Thank you for registering for this free online class! :)',
+        'rules_intro' => 'Here are few things to remember:',
+        'rules_1_part_1' => '1. Before joining your class session, ',
+        'rules_1_part_2' => 'please read all the materials',
+        'rules_1_part_3' => '.',
+        'rules_2' => '2. When it is time for the session to start, click the Zoom link to join the class.',
+        'rules_3_part_1' => '3. You should join the class ',
+        'rules_3_part_2' => 'five minutes before it starts',
+        'rules_3_part_3' => '.',
+        'rules_4' => '4. Be sure to join the session from laptop or desktop computer.',
+    ],
+    'registered_successfully' => 'You have successfully registered!',
+];
