02ccb85666035039297168aba0b9618d151596ef
imanueltantodaud <imanuel.tanto.daud@gmail.com>
2021-02-03 16:56:19 +0700

    (v.3.4.4.193)_update_layout_css_and_js_table_features_when_using_class_example1

--- resources/views/layouts/css_and_js/table.blade.php
+++ resources/views/layouts/css_and_js/table.blade.php
@@ -62,7 +62,23 @@
         'info'        : true,
         'autoWidth'   : false
       })
-      $('.example1').DataTable()
+      $('.example1').DataTable({
+        'deferRender'    : true,  // optimisasi untuk load data dalam jumlah besar
+        'processing'     : true,  // menunjukkan indikator pada waktu tabel masih proses load data
+        //'scrollX'        : true,  // show tabel pada viewport yang dapat di-scroll secara horizontal pada waktu tampilan tidak mencukupi (baik. Perlu disesuaikan secara manual mengenai width masing-masing kolom). Saat ini, fungsi ini di-disable, karena (sebagai pertimbangan), apakah user akan melakukan scrolling secara horizontal? Maka sebaiknya menunjukkan dulu bahwa ada fitur tersebut. Jika tidak/belum ditunjukkan, perkecil ukuran tabel khususnya untuk disesuaikan pada tampilan mobile dan tablet
+        'scrollY'        : '',    // default value adalah '', diisi unit CSS, untuk membatasi ukuran <tbody> pada height tertentu apabila diperlukan untuk dibatasi
+        'scrollCollapse' : false, // default value adalah false. Jika true, maka waktu menggunakan scrollY dan data berjumlah lebih sedikit, maka ukuran tabel akan menyesuaikan dengan ukuran yang disebutkan dalam parameter scrollY
+        'stateSave'      : false, // default: false. jika true, maka pada waktu tabel di-refresh, akan kembali ke state sebelum di-refresh, tidak kembali ke kondisi page 1 dan kondisi awal, kecuali jika ID tabel berubah, atau URL berubah (mohon diperhatikan, record waktu search juga tetap disimpan, mungkin inconvenience untuk beberapa user apabila memang tidak sedang diperlukan)
+        //'stateDuration'  : 7200,  // default: 7200. jika -1 maka sessionStorage digunakan, jika 0 atau lebih besar maka localStorage digunakan. Value diberikan dalam satuan seconds (s). Angka 0 berarti state dapat disimpan tanpa batas waktu. Gunakan opsi ini apabila fitur 'stateSave' bernilai true, bukan false
+        'lengthMenu'     : [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ], // digunakan untuk menentukan angka-angka pagination (10, 25, 50, 100, "All", dsb), untuk penulisan "All", dibuat 2 array: array ke-1 untuk simpan angka yang diperlukan, array ke-2 untuk simpan string yang ditampilkan sebagai front-end filter ke user
+        'pagingType'     : 'simple_numbers',  // jenis tampilan pagination. Default adalah 'simple_numbers' (tombol previous dan next, serta page number), untuk 'full_numbers', tombol first, previous, next, dan last, serta page number, sementara untuk 'first_last_numbers', tombol first dan last, serta page number (dan ada beberapa value lain). Untuk mengubah jumlah page number yang di-display pada pagination bar, jalankan kode ini ($.fn.DataTable.ext.pager.numbers_length = 9;) setelah library DataTables di-load (ya, dapat diterapkan pada file ini). untuk angka 9 dapat diganti bilangan ganjil, hanya boleh bilangan ganjil
+        'search'         : {
+          'caseInsensitive'  : true, // default value adalah true. jika false, maka fitur search dilakukan secara case-sensitive. jika true, maka huruf kapital dan kecil dianggap sama saja
+          'regex'            : true, // default value adalah false. jika true, maka fitur search dapat menggunakan query dalam bentuk regular expression (misal menggunakan tanda [A, B, C] untuk mencari row yang memiliki huruf A, B, ATAU C, tidak harus A, B, dan C). jika false, maka pencarian regex di-disable
+          'smart'            : true, // default value adalah true. jika false, maka fitur search hanya melakukan perbandingan string biasa, entah case-sensitive atau tidak (kecuali, jika diaktifkan opsi 'regex' menggantikan opsi ini, karena opsi smart filtering ini juga menggunakan complex regex)
+        },
+        'searchDelay'    : null,  // default value adalah null, dapat diganti integer (angka saja, bukan string) dengan satuan miliseconds (ms), misal 350 maka dianggap 350 ms. opsi ini digunakan untuk memberi delay pada waktu user menekan tombol keyboard di fitur search, per keypress. untuk value null, DataTable memproses sesuai dengan processing mode (jika client-side processing, instant atau 0 ms; jika server-side processing, 400 ms, sehingga lebih lambat, tetapi server-side dapat digunakan untuk mengoperasikan search pada row dalam jumlah besar, karena pencarian juga dibantu oleh mesin database dari sisi server). Boleh disesuaikan dengan kebutuhan
+      })
     })
   </script>
 @endpush
