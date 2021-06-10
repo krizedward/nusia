b58a6fee59b24ea2a648452000286805115941e7
imanueltantodaud <imanuel.tanto.daud@gmail.com>
2021-06-10 22:21:36 +0700

    (v.3.4.5.87)_update_bug_penjelasan_dalam_proses_registrasi_pada_role_student

--- resources/views/role_student/registration_06_complete_course_registrations.blade.php
+++ resources/views/role_student/registration_06_complete_course_registrations.blade.php
@@ -47,7 +47,7 @@
                               blue-colored text
                             </a>
                             to display a pop-up describing more information about the instructors!<br />
-                            <span style="color:#ff0000;">Contact us if you encounter a problem.</span>
+                            {{--<span style="color:#ff0000;">Contact us if you encounter a problem.</span>--}}
                           </dd>
                         </dl>
                         <hr>
